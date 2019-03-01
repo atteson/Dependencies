d32017f4f28f7b68d7fe4637e515795d3de43521
Kevin Atteson <kevin@atteson.com>
2019-03-01 12:29:05 -0500

    seems to work

--- src/Dependencies.jl
+++ src/Dependencies.jl
@@ -1,5 +1,7 @@
 module Dependencies
 
+export ArrayNode, ArrayTerminal
+
 abstract type AbstractArrayNode{T,N} end
 
 mutable struct ArrayNode{T,N} <: AbstractArrayNode{T,N}
@@ -21,14 +23,18 @@ function ArrayNode( T, size::NTuple{N,Int}, f::Function, parameters::AbstractArr
 end
 
 function setparameterof!( node::ArrayNode, parameter::AbstractArrayNode )
-    push!( parameter.parameterof( node ) )
+    push!( parameter.parameterof, node )
 end
 
 function (node::ArrayNode{T})() where {T}
-    if node.value == missing
-        node.value = zeros( T, node.size )
-        parametervalues = get.( node.parameters )
-        value = node.f!( node.value, parametervalues... )
+    if node.dirty
+        if ismissing( node.value )
+            node.value = zeros( T, node.size )
+        end
+
+        parametervalues = [parameter() for parameter in node.parameters]
+        node.value = node.f!( node.value, parametervalues... )
+        node.dirty = false
     end
     return node.value
 end
@@ -43,15 +49,13 @@ mutable struct ArrayTerminal{T,N} <: AbstractArrayNode{T,N}
     value::Union{Array{T,N},Missing}
 end
 
-Terminal( value::Array{T,N} ) where {T,N} = Terminal( AbstractArrayNode{T,N}[], value::Array{T,N} )
+ArrayTerminal( value::Array ) = ArrayTerminal( ArrayNode[], value )
 
 (t::ArrayTerminal)() = t.value
 
 function (t::ArrayTerminal{T,N})( value::Array{T,N} ) where {T,N}
     t.value = value
-    for parameterof in t.parametersof
-        invalidate!( parameterof )
-    end
+    invalidate!.( t.parameterof )
 end
 
 end # module
--- test/test.jl
+++ test/test.jl
@@ -1,1 +1,48 @@
 using Dependencies
+using Distributions
+using Random
+
+Random.seed!(1)
+p = 2
+T = 1000
+X = ArrayTerminal( rand( Normal(), T, p ) )
+betastar = rand( Normal(), 2 )
+y = ArrayTerminal( X()*betastar + rand( Normal(), T ) )
+XTXinvXT = ArrayNode( Float64, (p,T), (XTXinvXT, X) -> XTXinvXT = inv(X'*X)*X', X )
+
+@assert( ismissing( XTXinvXT.value ) )
+
+beta = ArrayNode( Float64, (p,), (beta,XTXinvXT,y) -> beta = XTXinvXT * y, XTXinvXT, y )
+
+@assert( XTXinvXT() == inv(X.value' * X.value)*X.value' )
+@assert( ismissing( beta.value ) )
+
+@assert( !XTXinvXT.dirty )
+@assert( beta.dirty )
+
+@assert( beta() == XTXinvXT.value * y.value )
+
+@assert( !XTXinvXT.dirty )
+@assert( !beta.dirty )
+
+y( X()*betastar + rand( Normal(), T ) )
+
+@assert( !XTXinvXT.dirty )
+@assert( beta.dirty )
+
+@assert( beta() == XTXinvXT.value * y.value )
+
+@assert( !XTXinvXT.dirty )
+@assert( !beta.dirty )
+
+X( rand( Normal(), T, p ) )
+
+@assert( XTXinvXT.dirty )
+@assert( beta.dirty )
+
+@assert( beta() == XTXinvXT.value * y.value )
+
+@assert( !XTXinvXT.dirty )
+@assert( !beta.dirty )
+
+
