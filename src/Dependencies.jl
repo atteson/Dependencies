c979f2f3b6b56093ade2e4949db4d77abc926a38
Kevin Atteson <kevin@atteson.com>
2019-05-28 13:20:24 -0400

    function node seems to be working

--- src/Dependencies.jl
+++ src/Dependencies.jl
@@ -1,5 +1,6 @@
 module Dependencies
 
+using Base64
 using Serialization
 
 const defaultdir = joinpath( dirname(dirname(pathof(Dependencies))), "data" )
@@ -10,13 +11,25 @@ struct FunctionNode{F1 <: Function, F2 <: Function}
     dir::String
 end
 
-FunctionNode( f::F1; hash::F2 = hash, dir::String = defaultdir ) where {F1 <: Function, F2 <: Function} =
+FunctionNode( f::F1; hash::F2 = hash, dir::String = defaultdir ) where {F1, F2} =
     FunctionNode( f, hash, dir )
 
-function (f::FunctionNode{F1,F2})( args... )
-    filename = joinpath( f.dir, f.hash( (f, args...) ) )
+function (f::FunctionNode{F1,F2})( args... ) where {F1, F2}
+    mkpath( f.dir )
+    filename = joinpath( f.dir, base64encode( f.hash( (f, args...) ) ) )
     if isfile( filename )
-        return readrecursive( 
+        io = open( filename, "r" )
+        object = deserialize( io )
+        close( io )
+    else
+        object = f.f( args... )
+
+        io = open( filename, "w" )
+        serialize( io, object )
+        close( io )
+    end
+    return object
+end
     
 
 end # module
---
+++ test/testfunctionnode.jl
@@ -0,0 +1,20 @@
+using Dependencies
+using Random
+using StatsBase
+
+function f( seed::Int, n::Int = 1_000_000 )
+    Random.seed!( seed )
+    mean( rand( n ) )
+end
+
+n = 1_000_000_000
+@time f( 1, n )
+@time f( 1, n )
+# 5.5s
+
+fnode = Dependencies.FunctionNode( f )
+
+@time fnode( 1, n )
+# 5.8s
+@time fnode( 1, n )
+# 0.0s
