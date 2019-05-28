88bc67cc1896c36d528b2633365e3c89f8dd6ac6
Kevin Atteson <kevin@atteson.com>
2019-05-28 12:43:26 -0400

    switching to serialization

--- src/Dependencies.jl
+++ src/Dependencies.jl
@@ -1,29 +1,22 @@
 module Dependencies
 
-recursivewrite( io::IO, x ) = write( io, x )
+using Serialization
 
-function recursivewrite( io::IO, v::Array{T,N} ) where {T,N}
-    sizes = size( v )
-    for i in 1:N
-        write( io, sizes[i] )
-    end
-    for i = 1:prod(sizes)
-        recursivewrite( io, v[i] )
-    end
+const defaultdir = joinpath( dirname(dirname(pathof(Dependencies))), "data" )
+
+struct FunctionNode{F1 <: Function, F2 <: Function}
+    f::F1
+    hash::F2
+    dir::String
 end
 
-recursiveread( io::IO, t::Type{T} ) where {T} = read( io, t )
+FunctionNode( f::F1; hash::F2 = hash, dir::String = defaultdir ) where {F1 <: Function, F2 <: Function} =
+    FunctionNode( f, hash, dir )
 
-function recursiveread( io::IO, ::Type{Array{T,N}} ) where {T,N}
-    sizes = Int[]
-    for i in 1:N
-        push!( sizes, Base.read( io, Int ) )
-    end
-    v = T[]
-    for i = 1:prod(sizes)
-        push!( v, recursiveread( io, T ) )
-    end
-    return v
-end
+function (f::FunctionNode{F1,F2})( args... )
+    filename = joinpath( f.dir, f.hash( (f, args...) ) )
+    if isfile( filename )
+        return readrecursive( 
+    
 
 end # module
--- test/testhash.jl
+++ test/testhash.jl
@@ -1,15 +1,15 @@
-using Dependencies
+using Serialization
 
 x0 = [rand(100) for i in 1:1000]
 
 file = tempname()
 
 f = open( file, "w" )
-Dependencies.recursivewrite( f, x0 )
+serialize( f, x0 )
 close(f)
 
 f = open( file, "r" )
-x1 = Dependencies.recursiveread( f, Vector{Vector{Float64}} )
+x1 = deserialize( f )
 close(f)
 
 @assert( hash(x0) == hash(x1) )
